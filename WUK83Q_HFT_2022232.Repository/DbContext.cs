0227f429cd2f75bab64b34597c0bbcdbb681d1c5
Pesti Balázs Csaba <pesti.balazs01@gmail.com>
2023-05-10 18:27:15 +0200

    DBContext osztály létrehozva a Repository rétegben

---
+++ WUK83Q_HFT_2022232.Repository/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WUK83Q_HFT_2022232.Repository
+{
+    internal class DbContext
+    {
+
+    }
+}
