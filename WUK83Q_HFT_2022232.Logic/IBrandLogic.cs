aa30e27b65a61bf7d2d5ebc5c30a898cae1441e2
Pesti Balázs Csaba <pesti.balazs01@gmail.com>
2023-05-26 23:46:10 +0200

    A Logic rétegben létre lett hozva az IBrandLogic interfész.

---
+++ WUK83Q_HFT_2022232.Logic/IBrandLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WUK83Q_HFT_2022232.Models;
+
+namespace WUK83Q_HFT_2022232.Logic
+{
+    public interface IBrandLogic
+    {
+        void Create(Brand item);
+        void Delete(int id);
+        Brand Read(int id);
+        IQueryable<Brand> ReadAll();
+        void Update(Brand item);
+
+    }
+}
