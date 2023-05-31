e8d5de6a86ed201615531e66e17b23d20fb7ce88
Pesti Balázs Csaba <pesti.balazs01@gmail.com>
2023-05-31 20:08:13 +0200

    DBSeed adatai javítva lettek.

--- WUK83Q_HFT_2022232.Models/Auto.cs
+++ WUK83Q_HFT_2022232.Models/Auto.cs
@@ -37,13 +37,14 @@ namespace WUK83Q_HFT_2022232.Models
             
         }
 
-        public Auto(string brand, string type, int vintage, int ownerId, int autoId)
+        public Auto(string brand, string type, int vintage, int ownerId, int autoId, int brandId)
         {
             AutoId = autoId;
             //Brand = brand;
             Type = type;
             OwnerId = ownerId;
             Vintage = vintage;
+            BrandId = brandId;
            
         }
 
--- WUK83Q_HFT_2022232.Repository/Database/AutoDbContext.cs
+++ WUK83Q_HFT_2022232.Repository/Database/AutoDbContext.cs
@@ -70,56 +70,56 @@ namespace WUK83Q_HFT_2022232.Repository
 
             modelBuilder.Entity<Auto>().HasData(new Auto[]
             {
-                new Auto("Kia", "Carens", 2008, 8, 111),
-                new Auto("BMW", "M4", 2014, 4, 112),
-                new Auto("Kia", "Ceed", 2013, 1, 113),
-                new Auto("Opel", "Corsa", 2016, 1, 114),
-                new Auto("Suzuki", "Swift", 2001, 2, 115),
-                new Auto("Mercedes-Benz", "C36 AMG", 2011, 3, 116),
-                new Auto("Audi", "A8", 2018, 3, 117),
-                new Auto("Volkswagen", "Golf", 2008, 4, 118),
-                new Auto("Toyota", "Avensis", 2007, 2, 119),
-                new Auto("Peugeot", "306", 2004, 5, 120),
-                new Auto("Renault", "Clio", 2015, 6, 121),
-                new Auto("Fiat", "Bravo", 2009, 7, 122),
-                new Auto("Toyota", "Corolla", 2017, 7, 123),
-                new Auto("Mercedes-Benz", "C63 AMG", 2016, 8, 124),
-                new Auto("Nissan", "Qashqai", 2010, 9, 125),
-                new Auto("Ferrari", "Roma", 2021, 10, 126),
-                new Auto("Mercedes-Benz", "G63 AMG", 2022, 10, 127),
-                new Auto("Alfa Romeo", "Giulia", 2018, 9, 128),
-                new Auto("Aston Martin", "DB9", 2005, 10, 129),
-                new Auto("Ford", "Mustang", 2013, 11, 130),
-                new Auto("Ford", "Kuga", 2014, 12, 131),
-                new Auto("Nissan", "Micra", 2013, 13, 132),
-                new Auto("Opel", "Insignia", 2013, 13, 133),
-                new Auto("BMW", "I3", 2013, 12, 134),
-                new Auto("Toyota", "Celica", 1992, 12, 135),
-                new Auto("Tesla", "Model S", 2016, 14, 136),
-                new Auto("Volkswagen", "ID.3", 2022, 14, 137),
-                new Auto("Cadillac", "Escalade", 2013, 15, 138)
+                new Auto("Kia", "Carens", 2008, 8, 111, 01),
+                new Auto("BMW", "M4", 2014, 4, 112, 04),
+                new Auto("Kia", "Ceed", 2013, 1, 113, 01),
+                new Auto("Opel", "Corsa", 2016, 1, 114, 05),
+                new Auto("Suzuki", "Swift", 2001, 2, 115, 06),
+                new Auto("Mercedes-Benz", "C36 AMG", 2011, 3, 116, 03),
+                new Auto("Audi", "A8", 2018, 3, 117, 07),
+                new Auto("Volkswagen", "Golf", 2008, 4, 118, 08),
+                new Auto("Toyota", "Avensis", 2007, 2, 119, 02),
+                new Auto("Peugeot", "306", 2004, 5, 120, 09),
+                new Auto("Renault", "Clio", 2015, 6, 121, 10),
+                new Auto("Fiat", "Bravo", 2009, 7, 122, 11),
+                new Auto("Toyota", "Corolla", 2017, 7, 123, 02),
+                new Auto("Mercedes-Benz", "C63 AMG", 2016, 8, 124, 03),
+                new Auto("Nissan", "Qashqai", 2010, 9, 125, 12),
+                new Auto("Ferrari", "Roma", 2021, 10, 126, 13),
+                new Auto("Mercedes-Benz", "G63 AMG", 2022, 10, 127, 03),
+                new Auto("Alfa Romeo", "Giulia", 2018, 9, 128, 14),
+                new Auto("Aston Martin", "DB9", 2005, 10, 129, 15),
+                new Auto("Ford", "Mustang", 2013, 11, 130, 16),
+                new Auto("Ford", "Kuga", 2014, 12, 131, 16),
+                new Auto("Nissan", "Micra", 2013, 13, 132, 12),
+                new Auto("Opel", "Insignia", 2013, 13, 133, 05),
+                new Auto("BMW", "I3", 2013, 12, 134, 04),
+                new Auto("Toyota", "Celica", 1992, 12, 135, 02),
+                new Auto("Tesla", "Model S", 2016, 14, 136, 17),
+                new Auto("Volkswagen", "ID.3", 2022, 14, 137, 08),
+                new Auto("Cadillac", "Escalade", 2013, 15, 138, 18)
             }) ;
 
             modelBuilder.Entity<Brand>().HasData(new Brand[]
             {
-                new Brand(01, "Kia", "South-Korea", 1944, true, false),
-                new Brand(02, "Toyota", "Japan", 1936, false, false),
-                new Brand(03, "Mercedes-Benz", "Germany", 1890, true, true),
-                new Brand(04, "BMW", "Germany", 1916, true, false),
-                new Brand(05, "Opel", "Germany", 1862, true, false),
-                new Brand(06, "Suzuki", "Japan", 1909, false, false),
-                new Brand(07, "Audi", "Germany", 1932, true, false),
-                new Brand(08, "Volkswagen", "Germany", 1937, true, false),
-                new Brand(09, "Peugeot", "France", 1810, true, false),
-                new Brand(10, "Renault", "France", 1889, true, false),
-                new Brand(11, "Fiat", "Italy", 1899, true, false),
-                new Brand(12, "Nissan", "Japan", 1930, true, false),
-                new Brand(13, "Ferrari", "Italy", 1947, false, true),
-                new Brand(14, "Alfa Romeo", "Italy", 1910, false, true),
-                new Brand(15, "Aston Martin", "England", 1913, false, true),
-                new Brand(16, "Ford", "Germany", 1903, true, false),
-                new Brand(17, "Tesla", "USA", 2003, true, false),
-                new Brand(18, "Cadillac", "USA", 1902, true, false),
+                new Brand(01, "Kia", "South-Korea", 1944, true, false, 010),
+                new Brand(02, "Toyota", "Japan", 1936, false, false, 003),
+                new Brand(03, "Mercedes-Benz", "Germany", 1890, true, true, 004),
+                new Brand(04, "BMW", "Germany", 1916, true, false, 006),
+                new Brand(05, "Opel", "Germany", 1862, true, false, 001),
+                new Brand(06, "Suzuki", "Japan", 1909, false, false, 013),
+                new Brand(07, "Audi", "Germany", 1932, true, false, 002),
+                new Brand(08, "Volkswagen", "Germany", 1937, true, false, 002),
+                new Brand(09, "Peugeot", "France", 1810, true, false, 001),
+                new Brand(10, "Renault", "France", 1889, true, false, 011),
+                new Brand(11, "Fiat", "Italy", 1899, true, false, 001),
+                new Brand(12, "Nissan", "Japan", 1930, true, false, 011),
+                new Brand(13, "Ferrari", "Italy", 1947, false, true, 001),
+                new Brand(14, "Alfa Romeo", "Italy", 1910, false, true, 001),
+                new Brand(15, "Aston Martin", "England", 1913, false, true, 015),
+                new Brand(16, "Ford", "Germany", 1903, true, false, 005),
+                new Brand(17, "Tesla", "USA", 2003, true, false, 014),
+                new Brand(18, "Cadillac", "USA", 1902, true, false, 008),
             }) ;
 
             modelBuilder.Entity<Concern>().HasData(new Concern[]{
@@ -137,6 +137,7 @@ namespace WUK83Q_HFT_2022232.Repository
                 new Concern(012, "Geely", 2017, "France-Japan", 13),
                 new Concern(013, "Suzuki", 1909, "Japan", 22),
                 new Concern(014, "Tesla", 2003, "USA", 19),
+                new Concern(015, "Aston Martin", 1913, "England", 23),
 
 
 
@@ -152,6 +153,17 @@ namespace WUK83Q_HFT_2022232.Repository
             //    BornOfConcern = bornOfConcern;
             //    CountryOfConcern = countryOfTheConcern;
             //    PositionInRanking = positionInRanking;
+            //}
+
+            //public Brand(int brandId, string brandName, string originOfBrand, int bornOfBrand, bool isProducingFullyElectricCars, bool hasFormula1Team, int concernId)
+            //{
+            //    BrandId = brandId;
+            //    BrandName = brandName;
+            //    OriginOfBrand = originOfBrand;
+            //    BornOfBrand = bornOfBrand;
+            //    IsProducingFullyElectricCars = isProducingFullyElectricCars;
+            //    HasFormula1Team = hasFormula1Team;
+            //    ConcernId = concernId;
             //}
         }
     }
